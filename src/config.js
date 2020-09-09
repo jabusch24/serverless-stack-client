007591fac243537f7b402adc78b0ed1c820681e1
Jannis <jannis.busch@bayer.com>
2020-09-09 10:17:47 +0200

    Setting up our React App and Amplify config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "bayronnect-notes-app",
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://4dk4rpmq23.execute-api.eu-central-1.amazonaws.com/dev",
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_imcHiXdIr",
+    APP_CLIENT_ID: "5oq6dqhorm1hk6bl3jjm2891",
+    IDENTITY_POOL_ID: "eu-central-1:ae208599-2f07-42ae-b300-a870607ce3f8",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Amplify } from "aws-amplify";
+import config from "./config";
+
+// from: https://serverless-stack.com/chapters/configure-aws-amplify.html
+
+// configure simply sets the configuration for later interactions
+Amplify.configure({
+  // Auth === Cognito
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  // Storage === S3
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  // API === API Gateway
+  API: {
+    endpoints: [
+      {
+        name: "notes",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
